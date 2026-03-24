fdad28de9849b4e8c895d60564b9297653c2d497
Weishi Zeng <weishi@imbue.com>
2026-03-23 18:38:42 -0700

    Fix issues from vet review

--- qutebrowser/browser/shared.py
+++ qutebrowser/browser/shared.py
@@ -286,7 +286,7 @@ def get_user_stylesheet(searching=False):
             css += f.read()
 
     if (config.val.scrolling.bar == 'never' or
-            config.val.scrolling.bar == 'when-searching' and not searching or
+            (config.val.scrolling.bar == 'when-searching' and not searching) or
             config.val.scrolling.bar == 'overlay'):
         css += '\nhtml > ::-webkit-scrollbar { width: 0px; height: 0px; }'
 
